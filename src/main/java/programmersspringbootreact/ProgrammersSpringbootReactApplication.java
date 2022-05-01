809c7835ac4cd8c854b052e7446de3dcaad9697e
yanJuicy <hanjub3@gmail.com>
2022-05-02 01:48:17 +0900

    initial commit

---
+++ src/main/java/programmersspringbootreact/ProgrammersSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package programmersspringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersSpringbootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammersSpringbootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/programmersspringbootreact/ProgrammersSpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package programmersspringbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersSpringbootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
