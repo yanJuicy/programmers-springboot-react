c4fe9be8c21fe38b3431bfd691e7b3bb49924b84
yanJuicy <hanjub3@gmail.com>
2022-05-02 11:18:30 +0900

    ProductJdbcRepositoryTest 클래스 Test DB 연결 설정

---
+++ src/test/java/programmersspringbootreact/repository/ProductJdbcRepositoryTest.java
@@ -0,0 +1,54 @@
+package programmersspringbootreact.repository;
+
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import programmersspringbootreact.model.Category;
+import programmersspringbootreact.model.Product;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProductJdbcRepositoryTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        var config = aMysqldConfig(Version.v8_0_11)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+
+        embeddedMysql = anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+    }
+
+    @AfterAll
+    static void cleanup() {
+        embeddedMysql.stop();
+    }
+
+
+    @Autowired
+    ProductRepository repository;
+
+
+
+
+
+}
