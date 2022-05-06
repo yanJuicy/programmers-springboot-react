2d3ca2ed4008cfd79c0e2530422b04ff075febed
yanJuicy <hanjub3@gmail.com>
2022-05-07 06:21:49 +0900

    OrderStatus 주문 상태 축소

--- src/main/java/programmersspringbootreact/order/model/OrderStatus.java
+++ src/main/java/programmersspringbootreact/order/model/OrderStatus.java
@@ -1,10 +1,7 @@
 package programmersspringbootreact.order.model;
 
 public enum OrderStatus {
-    ACCEPTED,
-    PAYMENT_CONFIRMED,
-    READY_FOR_DELIVERY,
-    SHIPPED,
-    SETTLED,
-    CANCELED
+    ACCEPTED,   // 주문 확인
+    READY_FOR_DELIVERY, // 배송 준비 (오후 2시 이후)
+    SHIPPED,    // 배송 완료
 }
