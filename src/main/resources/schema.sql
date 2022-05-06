767a1e9f850c700a8b6a7b043234029590a70988
yanJuicy <hanjub3@gmail.com>
2022-05-07 04:38:10 +0900

    ORDRES, ORDER_ITEMS 테이블 생성 sql 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,4 +6,28 @@ CREATE TABLE products (
     description     varchar(500)    default null ,
     created_at      datetime(6)     not null ,
     updated_at      datetime(6)     default null
-)
+);
+
+CREATE TABLE orders(
+    order_id        binary(16)      primary key,
+    email           varchar(30)     not null,
+    address         varchar(200)     not null,
+    postcode        varchar(200)     not null,
+    order_status    varchar(50)     not null,
+    created_at      datetime(6)     not null ,
+    updated_at      datetime(6)     default null
+);
+
+CREATE TABLE order_items(
+    seq             bigint          not null primary key auto_increment,
+    order_id        binary(16)      not null,
+    product_id      binary(16)      not null,
+    category        varchar(50)     not null,
+    price           bigint          not null,
+    quantity        int             not null,
+    created_at      datetime(6)     not null ,
+    updated_at      datetime(6)     default null,
+    index(order_id),
+    constraint fk_order_items_to_order foreign key (order_id) references orders(order_id) on delete cascade ,
+    constraint fk_order_items_to_product foreign key (product_id) references products(product_id)
+);
