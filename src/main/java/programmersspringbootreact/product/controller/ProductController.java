69a19e2d61540337a34ce71313ff7cf514b37000
yanJuicy <hanjub3@gmail.com>
2022-05-08 18:51:57 +0900

    ProductController Product 업데이트 Get, Post 기능 추가

--- src/main/java/programmersspringbootreact/product/controller/ProductController.java
+++ src/main/java/programmersspringbootreact/product/controller/ProductController.java
@@ -2,10 +2,8 @@ package programmersspringbootreact.product.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import programmersspringbootreact.product.model.Product;
 import programmersspringbootreact.product.service.ProductService;
 
 import java.util.UUID;
@@ -44,6 +42,21 @@ public class ProductController {
     @DeleteMapping("/products/{uuid}")
     public String productDelete(@PathVariable UUID uuid) {
         productService.deleteById(uuid);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/{uuid}")
+    public String getProductUpdatePage(@PathVariable UUID uuid, Model model) {
+        Product product = productService.getProductById(uuid);
+        model.addAttribute("product", product);
+        return "update-product";
+    }
+
+    @PutMapping("/products/{uuid}")
+    public String productUpdate(@PathVariable UUID uuid, UpdateProductRequestDto updateProductRequestDto) {
+        Product product = productService.getProductById(uuid);
+        product.update(updateProductRequestDto);
+        productService.updateProduct(product);
         return "redirect:/products";
     }
 
