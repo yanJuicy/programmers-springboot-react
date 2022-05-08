9ddd3dde5c9e056a8c97477f7eceac7adf177169
yanJuicy <hanjub3@gmail.com>
2022-05-08 22:14:35 +0900

    OrderController, 주문한 상품들을 볼 수 있는 기능 추가

--- src/main/java/programmersspringbootreact/order/controller/OrderController.java
+++ src/main/java/programmersspringbootreact/order/controller/OrderController.java
@@ -3,7 +3,10 @@ package programmersspringbootreact.order.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import programmersspringbootreact.order.service.OrderService;
+
+import java.util.UUID;
 
 @Controller
 public class OrderController {
@@ -19,6 +22,14 @@ public class OrderController {
         var orders = orderService.getAllOrders();
         model.addAttribute("orders", orders);
         return "order-list";
+    }
+
+    @GetMapping("/orders/{orderId}")
+    public String orderDetailPage(@PathVariable UUID orderId, Model model) {
+        var orderItems = orderService.getAllOrderItemsById(orderId);
+        model.addAttribute("orderId", orderId);
+        model.addAttribute("orderItems", orderItems);
+        return "order-detail";
     }
 
 }
