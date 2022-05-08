82e9fe93bc01b83391b9a264706500877f0d1c6a
yanJuicy <hanjub3@gmail.com>
2022-05-08 17:53:29 +0900

    ProductController, 상품 삭제 api 추가

--- src/main/java/programmersspringbootreact/product/controller/ProductController.java
+++ src/main/java/programmersspringbootreact/product/controller/ProductController.java
@@ -2,9 +2,13 @@ package programmersspringbootreact.product.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import programmersspringbootreact.product.service.ProductService;
+
+import java.util.UUID;
 
 @Controller
 public class ProductController {
@@ -34,6 +38,12 @@ public class ProductController {
                 createProductRequest.getCategory(),
                 createProductRequest.getPrice(),
                 createProductRequest.getDescription());
+        return "redirect:/products";
+    }
+
+    @DeleteMapping("/products/{uuid}")
+    public String productDelete(@PathVariable UUID uuid) {
+        productService.deleteById(uuid);
         return "redirect:/products";
     }
 
